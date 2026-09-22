893dc8eef3123aa4fc9ec98216a7a63fec153b77
codebaba007 <mehedi2hassan2003@gmail.com>
2026-09-22 12:26:17 +0600

    Complete Day 74 TypeScript basics

---
+++ Day-74-TypeScript-Basics/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let studentName = "Mehedi";
+let age = 25;
+let isStudent = true;
+console.log(studentName);
+console.log(age);
+console.log(isStudent);
---
+++ Day-74-TypeScript-Basics/index.ts
@@ -0,0 +1,17 @@
+let studentName: string = "Mehedi";
+
+let age: number = 25;
+
+let isStudent: boolean = true;
+
+console.log(studentName);
+console.log(age);
+console.log(isStudent);
+
+let university = "UITS";
+let semester = 7;
+let graduated = false;
+
+console.log(university);
+console.log(semester);
+console.log(graduated);
